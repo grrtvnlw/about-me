64d8433e5543313ad3702f9e1957d69155bf8f88
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-02-19 13:05:14 -0500

    updated img and added JS file

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/gerrit2.jpg') {
+        myImage.setAttribute ('src', 'images/gerrit.jpg');
+    } else {
+        myImage.setAttribute('src', 'images/gerrit2.jpg');
+    }
+};
